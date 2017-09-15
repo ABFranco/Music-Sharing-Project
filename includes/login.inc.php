553aeadca82c6ae3a6c013ebf2d5597d62eea8b4
ABFranco <abfranco4@gmail.com>
2017-09-14 23:48:29 -0700

    after tutorial

---
+++ includes/login.inc.php
@@ -0,0 +1,48 @@
+<?php
+
+session_start();
+
+if (isset($_POST['submit'])) {
+	
+	include 'dbh.inc.php';
+
+	// even though called uid, you can also login with your email
+	$uid = mysqli_real_escape_string($conn, $_POST['uid']); 
+	$pwd = mysqli_real_escape_string($conn, $_POST['pwd']);
+
+	// error handlers
+	// check if inputs are empty
+	if (empty($uid) || empty($pwd)) {
+		header("Location: ../index.php?login=empty");
+		exit();
+	} else {
+		$sql = "SELECT * FROM users WHERE username = '$uid' OR email='$uid'";
+		$result = mysqli_query($conn, $sql);
+		$resultCheck = mysqli_num_rows($result);
+		if ($resultCheck < 1) {
+			// no user found
+			header("Location: ../index.php?login=error");
+			exit();
+		} else {
+			if ($row = mysqli_fetch_assoc($result)) {
+				// de-hashing password aka check if password matches the hashed version in DB
+				$hashedPwdCheck = password_verify($pwd, $row['password']);
+				if ($hashedPwdCheck == false) {
+					header("Location: ../index.php?login=error");
+					exit();
+				} elseif ($hashedPwdCheck == true) {
+					// log in the user
+					$_SESSION['u_id'] = $row['id'];
+					$_SESSION['u_email'] = $row['email'];
+					$_SESSION['u_uid'] = $row['username'];
+					header("Location: ../index.php?login=success");
+					exit();
+				}
+			}
+		}
+	}
+
+} else {
+	header("Location: ../index.php?login=error");
+	exit();
+}
---
+++ includes/signup.inc.php
@@ -0,0 +1,54 @@
+<?php
+
+if (isset($_POST['submit'])) {
+
+	include_once 'dbh.inc.php';
+
+	// makes sure people can't send in code with mysql_real....
+	$email = mysqli_real_escape_string($conn, $_POST['email']); 
+	$uid = mysqli_real_escape_string($conn, $_POST['uid']); 
+	$pwd = mysqli_real_escape_string($conn, $_POST['pwd']); 
+	$first = mysqli_real_escape_string($conn, $_POST['first']); 
+	$last = mysqli_real_escape_string($conn, $_POST['last']); 
+
+	// error handlers
+	// check for empty fields
+	if (empty($email) || empty($uid) || empty($pwd)) {
+		header("Location: ../signup.php?signup=empty");
+		exit();
+	} else {
+		// check if input characters are valid
+		if (!preg_match("/^[a-zA-Z]*$/", $first) || !preg_match("/^[a-zA-Z]*$/", $last)) {
+			header("Location: ../signup.php?signup=invalid");
+			exit();
+		} else {
+			// check if email is valid
+			if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+				header("Location: ../signup.php?signup=email");
+				exit();
+			} else {
+				// check if username already taken
+				$sql = "SELECT * FROM users WHERE username='$uid'";
+				$result = mysqli_query($conn, $sql);
+				$resultCheck = mysqli_num_rows($result);
+
+				if ($resultCheck > 0) {
+					header("Location: ../signup.php?signup=usertaken");
+					exit();
+				} else {
+					// hashing the password
+					$hashedPwd = password_hash($pwd, PASSWORD_DEFAULT);
+					// insert the user into the database
+					$sql = "INSERT INTO users (email, username, password, first_name, last_name) VALUES ('$email', '$uid', '$hashedPwd', '$first', '$last');";
+					mysqli_query($conn, $sql);
+					header("Location: ../signup.php?signup=success");
+					exit();
+				}
+			}
+		}
+	}
+
+} else {
+	header("Location: ../signup.php");
+	exit();
+}
